54cbdb9e6eb4ca036a5a40d5e442babdcbfc2472
Tung-Yen Wang <tw565@cornell.edu>
2025-01-22 18:01:30 -0500

    pwa modifications

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,35 +5,38 @@ const urlsToCache = [
   '/manifest.json',
   '/favicon.ico',
   '/logo192.png',
-  '/logo512.png'
+  '/logo512.png',
+  '/static/css/main.*.css',
+  '/static/js/main.*.js'
 ];
 
-self.addEventListener('install', (event) => {
+// Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Cached offline page during install');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request)
-          .then((response) => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
+    fetch(event.request)
+      .then(function(response) {
+        // If response was successful, clone it and store it in the cache
+        if (response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, responseClone);
           });
+        }
+        return response;
+      })
+      .catch(function() {
+        // If the network request failed, try to get it from the cache
+        return caches.match(event.request);
       })
   );
 }); 
