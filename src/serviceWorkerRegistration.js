65d8060edbb18c0c2be56706d54d217f7761215d
Tung-Yen Wang <tw565@cornell.edu>
2025-01-22 18:07:30 -0500

    size change

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,14 +1,17 @@
+// This optional code is used to register a service worker.
+// register() is not called by default.
+
 export function register() {
-  if ('serviceWorker' in navigator) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       navigator.serviceWorker
         .register(swUrl)
-        .then((registration) => {
-          console.log('ServiceWorker registration successful');
+        .then(registration => {
+          console.log('SW registered: ', registration);
         })
-        .catch((error) => {
-          console.error('Error during service worker registration:', error);
+        .catch(registrationError => {
+          console.log('SW registration failed: ', registrationError);
         });
     });
   }
@@ -17,10 +20,10 @@ export function register() {
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready
-      .then((registration) => {
+      .then(registration => {
         registration.unregister();
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error.message);
       });
   }
