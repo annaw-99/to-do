1ca175b74c1743ef776c5d3b119899d4cd6a6440
Tung-Yen Wang <annawang9909@gmail.com>
2025-02-10 13:38:48 -0500

    modified window size

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,6 @@ const urlsToCache = [
   '/logo512.png'
 ];
 
-// Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -16,7 +15,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
